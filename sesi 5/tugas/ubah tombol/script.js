c156538b9567cbad5399bc7b9e693eaa2cac2528
Ade Kurniawan <adekurniawan59375@gmail.com>
2023-08-10 11:13:40 +0700

    update sesi

---
+++ sesi 5/tugas/ubah tombol/script.js
@@ -0,0 +1,8 @@
+function ubahText() {
+    var element = document.getElementById('tombol')
+    if (element.innerHTML == 'klik aku') {
+        element.innerHTML = 'halo Yal'
+    } else {
+        element.innerHTML = 'klik aku'
+    }
+}
---
+++ sesi 6/DOM/dom.js
@@ -0,0 +1,2 @@
+document.write("hello yal");
+document.write("<h2>saya sedang belajar dom</h2>");
